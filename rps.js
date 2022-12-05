7f0739bbfd9aed1b4e3fc4aa4e9b9f13ba28c76c
Austin Clark <austin@pop-os.localdomain>
2022-12-04 19:03:01 -0700

    Initial Commit

---
+++ rps.js
@@ -0,0 +1,66 @@
+function getComputerChoice() {
+    const arr = ["ROCK", "PAPER", "SCISSORS"];
+    const randomIndex = Math.floor(Math.random() * arr.length);
+    const choice = arr[randomIndex];
+    return choice;
+}
+
+function playRound(playerSelection, computerSelection) {
+    playerSelection = prompt("Make Your Choice")
+    computerSelection = getComputerChoice();
+    let player = playerSelection.toUpperCase();
+    let comp = computerSelection
+    let result;
+    if (player == "ROCK") {
+        if (comp == "PAPER") {
+            result = "Paper, you lose"
+        } else if (comp == "SCISSORS") {
+            result = "Scissors, you win"
+        } else if (comp == "ROCK") {
+            result = "Rock, draw"
+        }
+    } else if (player == "PAPER") {
+        if (comp == "PAPER") {
+            result = "Paper, draw"
+        } else if (comp == "SCISSORS") {
+            result = "Scissors, you lose"
+        } else if (comp == "ROCK") {
+            result = "Rock, you win"
+        }
+    } else if (player == "SCISSORS") {
+        if (comp == "PAPER") {
+            result = "Paper, you win"
+        } else if (comp == "SCISSORS") {
+            result = "Scissors, draw"
+        } else if (comp == "ROCK") {
+            result = "Rock, you lose"
+        }
+    } else {
+        return "Error, please check spelling";
+    }
+    return result;
+}
+
+function game() {
+    let win = 0
+    let lose = 0
+    let score = `Score: ${win} to ${lose}`
+    for (let i = 0; i < 5; i++) {
+        let result = playRound();
+        if (result.includes("win")) {
+            win++;
+            console.log(score)
+        } else if (result.includes("lose")) {
+            lose++
+            console.log(score)
+        }
+        
+    }
+    if (win > lose) {
+        console.log("You Win!")
+    } else if (win < lose) {
+        console.log("You Lose")
+    } else if (win == lose) {
+        console.log("Draw")
+    }
+}
